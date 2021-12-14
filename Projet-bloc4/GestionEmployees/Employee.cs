ca60a245e0270edacfa07a144f273f43ddaeed56
eloodieb <eloodie.bouget@gmail.com>
2021-12-14 18:37:45 +0100

    Add GestionnaireEmployees

---
+++ Projet-bloc4/GestionEmployees/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet_bloc4.GestionEmployees
+{
+    public class Employee
+    {
+        int id;
+        string name;
+        string firstname;
+        string mobilePhone;
+        string phoneNumber;
+        string email;
+        string service;
+        string site;
+        DateTime creationDate;
+        DateTime updateDate;
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Firstname { get => firstname; set => firstname = value; }
+        public string MobilePhone { get => mobilePhone; set => mobilePhone = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public string Email { get => email; set => email = value; }
+        public string Service { get => service; set => service = value; }
+        public string Site { get => site; set => site = value; }
+        public DateTime CreationDate { get => creationDate; set => creationDate = value; }
+        public DateTime UpdateDate { get => updateDate; set => updateDate = value; }
+    }
+}
